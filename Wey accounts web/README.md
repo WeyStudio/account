7295b3d7c2bc40dee031d837e9850478157e8d35
Liu Shi an（Wey Studio） <122614217+Lt2023@users.noreply.github.com>
2024-02-07 19:45:12 +0800

    Add files via upload

---
+++ Wey accounts web/README.md
@@ -0,0 +1,18 @@
+# 打包项目在`dist`目录下
+
+# 启动项目
+
+1. 安装依赖
+```bash
+npm install
+```
+
+2. 运行项目
+```bash
+npm run dev
+```
+
+3. 打包项目
+```bash
+npm run build
+```
---
+++ Wey accounts web/src/api/index.js
@@ -0,0 +1,7 @@
+import request from './request'
+import queryString from 'query-string'
+
+// 发送验证码接口
+export const reqSendEmailCode = (email) => request({ url: '/send', method: 'post', data: queryString.stringify(email) })
+// 注册接口
+export const reqRegister = (data) => request({ url: '/register', method: 'post', data: queryString.stringify(data) })
